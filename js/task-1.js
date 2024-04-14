11504e2220934367d618fed7626b142daff623f4
Parhomuk_Pavlo <pavelparxomuk@gmail.com>
2024-04-14 14:32:22 +0300

    add new files

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,10 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-let totalPrice = quantity * pricePerDroid;
-if (totalPrice > customerCredits) {
+  let totalPrice = quantity * pricePerDroid;
+  if (totalPrice > customerCredits) {
     return `Insufficient funds!`;
-}
-else {
+  } else {
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-    }
+  }
 }
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
